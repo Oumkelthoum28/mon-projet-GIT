8459ed32634b37123f7a0175e5b89ea04475d7ca
Oumkelthoum28 <24253@Supnum.mr>
2025-11-03 23:19:20 +0000

    fix: corriger les erreurs dans config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuration du projet
+const config = {
+  appName: "Mon Projet Git",
+  version: "1.0.0",
+  author: "Oum Kelthoum Lemrabott"
+};
+
+console.log("Configuration chargée :", config);
